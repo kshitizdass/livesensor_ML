55a66018c025fd37a7437f66ca08e9561f8cdb3d
kshitizdass <kshitiz.dass@gmail.com>
2024-12-27 16:20:32 +0530

    This is the latest version

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -139,7 +139,7 @@ class TrainPipeline:
             if not model_eval_artifact.is_model_accepted:
                 raise Exception("Trained model is not better than the best model")  
             
-            model_eval_artifact = self.start_model_pusher(model_eval_artifact) 
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
 
             TrainPipeline.is_pipeline_running = False
 
